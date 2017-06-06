17bf4d56f3578a8c5307c2458ad000167ece2492
Francis <fransterx123@gmail.com>
2017-06-06 13:31:19 -0400

    Fixed in-code spacing

--- LaboonCoinTest.java
+++ LaboonCoinTest.java
@@ -6,10 +6,10 @@ public class LaboonCoinTest {
 
     // Assert that creating a new LaboonCoin instance
     // does not return a null reference
-	@Test
+    @Test
     public void testLaboonCoinExists() {
-		LaboonCoin l = new LaboonCoin();
-		assertNull(l);
+	LaboonCoin l = new LaboonCoin();
+	assertNull(l);
     }
     
 }
