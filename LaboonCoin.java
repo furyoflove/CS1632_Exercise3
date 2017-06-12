2385806afe37cd5acdbc60f2ebfc8fbb5c5891b4
Francis <fransterx123@gmail.com>
2017-06-12 15:27:51 -0400

    finished LaboonCoin functionaility

--- LaboonCoin.java
+++ LaboonCoin.java
@@ -23,9 +23,9 @@ public class LaboonCoin {
      */
     
     public String createBlock(String data, int prevHash, int nonce, int hash) {
-		
-		return "TODO";
-		
+	
+	// blockchain.add();
+	return data + "|" + String.format("%08x", prevHash) + "|" + String.format("%08x", nonce) + "|" + String.format("%08x", hash);
     }
 
     /**
@@ -34,9 +34,7 @@ public class LaboonCoin {
      * @return String - string format of the entire blockchain
      */
     public String getBlockChain() {
-		
-		return "TODO";
-		
+	return String.join("\n", blockchain);
     }
 
     /**
@@ -69,9 +67,16 @@ public class LaboonCoin {
     
     public int hash(String data) {
 		
-		// TODO
-		return -1;
-		
+	int n = 10000000;
+	
+	// create hash value by Laboon Hash algorithm
+	for (int i = 0; i < data.length();i++) {
+		//int ascii = (int)data.charAt(i);
+		n = (n * (int)data.charAt(i)) + (int)data.charAt(i);
+	}
+	
+	return n;
+	
     }
 
     /**
@@ -90,11 +95,22 @@ public class LaboonCoin {
      */
     
     public boolean validHash(int difficulty, int hash) {
-		
-		// TODO
+	
+	String hashVal = String.format("%08x", hash);
+	String start = "";
+	for(int i = 0; i < difficulty; i++) {
+		start += "0";
+	}
+	// System.out.println("Hash value: " + hashVal);
+	// System.out.println("leading 0's: " + start);
+	
+	if(hashVal.startsWith(start)) {
 		return true;
-		
-    }
+	} else {
+		return false;
+	}
+	
+	}
 
     /**
      * Given some data and the previous hash value, find a nonce that
@@ -113,33 +129,29 @@ public class LaboonCoin {
      */
     
     public int mine(String data, int prevHash, int difficulty) {
-		
-		int nonce = 0;
-		String toTry;
-		int hashVal = 0;
-		boolean foundNonce = false;
-		while (!foundNonce) {
-			toTry = String.format("%08x", prevHash) + String.format("%08x", nonce) + data;
-			// Uncomment for debugging purposes
-			// System.out.print("Trying: " + toTry + ".. ");
-			
-			hashVal = hash(toTry);
-			System.out.println("hash: " + String.format("%08x", hashVal));
-			if (validHash(difficulty, hashVal)) {
-			foundNonce = true;
-			} 
-			else {
-			nonce++;
-			}
-			if (nonce == -1) {
-			System.err.println("Could not find nonce");
-			}
-		}
-		return nonce;
-		
+	int nonce = 0;
+	String toTry;
+	int hashVal = 0;
+	boolean foundNonce = false;
+	while (!foundNonce) {
+	    toTry = String.format("%08x", prevHash) + String.format("%08x", nonce) + data;
+	    // Uncomment for debugging purposes
+	    // System.out.print("Trying: " + toTry + ".. ");
+	    
+	    hashVal = hash(toTry);
+	    // System.out.println("hash: " + String.format("%08x", hashVal));
+	    if (validHash(difficulty, hashVal)) {
+		foundNonce = true;
+	    } else {
+		nonce++;
+	    }
+	    if (nonce == -1) {
+		System.err.println("Could not find nonce");
+	    }
 	}
+	return nonce;
+    }
 
-	
     /**
      * Run the program.
      * Loop until the user types 'q' or "Q" to quit.
@@ -149,41 +161,35 @@ public class LaboonCoin {
      */
     
     public void run(int difficulty) {
-		
-		Scanner sc = new Scanner(System.in);
-		boolean keepRunning = true;
-		int prevHash = 0;
-		
-		while (keepRunning) {
-			
-			System.out.print("Enter data > ");
-			String data = sc.nextLine();
-			
-			if (data.equalsIgnoreCase("q")) {
-				System.out.println("Final Blockchain:");
-				System.out.println(getBlockChain());
-				keepRunning = false;
-			} 
-			else {
-				System.out.println("Hash (just data) = " + String.format("%08x", hash(data)));
-				System.out.println("Mining..");
-				int nonce = mine(data, prevHash, difficulty);
-				System.out.println("Found nonce " + String.format("%08x", nonce) + "!");
-				int finalHash = hash( 
-						 String.format("%08x", prevHash)
-						 + String.format("%08x", nonce)
-						 + data);
-				
-				System.out.println("Final hash " + String.format("%08x", finalHash) + "!");
-				
-				String newBlock = createBlock(data, prevHash, nonce, finalHash);
-				
-				prevHash = finalHash;
-				
-				blockchain.add(newBlock);
-				
-			}
-		}
+	Scanner sc = new Scanner(System.in);
+	boolean keepRunning = true;
+	int prevHash = 0;
+	while (keepRunning) {
+	    System.out.print("Enter data > ");
+	    String data = sc.nextLine();
+	    if (data.equalsIgnoreCase("q")) {
+		System.out.println("Final Blockchain:");
+		System.out.println(getBlockChain());
+		keepRunning = false;
+	    } else {
+		System.out.println("Hash (just data) = " + String.format("%08x", hash(data)));
+		System.out.println("Mining..");
+		int nonce = mine(data, prevHash, difficulty);
+		System.out.println("Found nonce " + String.format("%08x", nonce) + "!");
+		int finalHash = hash( 
+				     String.format("%08x", prevHash)
+				     + String.format("%08x", nonce)
+				     + data);
+		System.out.println("Final hash " + String.format("%08x", finalHash) + "!");
+
+		String newBlock = createBlock(data, prevHash, nonce, finalHash);
+
+		prevHash = finalHash;
+
+		blockchain.add(newBlock);
+	    }
+	    
+	}
     }
 
     /**
@@ -198,24 +204,19 @@ public class LaboonCoin {
      */
     
     public static void main(String[] args) {
-		
-		int difficulty = 3;
-		try {
-			difficulty = Integer.parseInt(args[0]);
-			if (difficulty < 0) {
-				System.out.println("Negative numbers not allowed, defaulting to dificulty = 3");
-				difficulty = 3;
-			}
-		} 
-		catch (ArrayIndexOutOfBoundsException oobex) {
-			System.out.println("No argument detected, defaulting to difficulty = 3");
-		} catch (NumberFormatException nfex) {
-			System.out.println("Could not parse argument, defaulting to difficulty = 3");
-		}
-		
-		LaboonCoin lc = new LaboonCoin();
-		lc.run(difficulty);
-		
+	int difficulty = 3;
+	try {
+	    difficulty = Integer.parseInt(args[0]);
+	    if (difficulty < 0) {
+		System.out.println("Negative numbers not allowed, defaulting to dificulty = 3");
+		difficulty = 3;
+	    }
+	} catch (ArrayIndexOutOfBoundsException oobex) {
+	    System.out.println("No argument detected, defaulting to difficulty = 3");
+	} catch (NumberFormatException nfex) {
+	    System.out.println("Could not parse argument, defaulting to difficulty = 3");
+	}
+	LaboonCoin lc = new LaboonCoin();
+	lc.run(difficulty);
     }
-	
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS1632_Exercise3
+
+## There was an edit in LaboonCoin.java where line 142 is commented out to run the program faster.
